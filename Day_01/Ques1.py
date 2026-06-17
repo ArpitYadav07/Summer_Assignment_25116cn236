2ca73181566aa39e4f60e571cc3767e1b54e9520
ArpitYadav07 <roleplay892000@gmail.com>
2026-06-17 18:03:10 +0530

    Added Day 1 Assignment

--- Day_01/Ques1.py
+++ Day_01/Ques1.py
@@ -1,6 +1,7 @@
 # Write a program to Calculate sum of first N natural numbers
 n=int(input("Enter the number "))
 sum=0
+
 for i in range(1,n+1):
     sum=sum+i
 print(f"The sum of {n} natural number is {sum}")
